5d540dc797496b581d20009279bc067e74483885
davidhostios <davidhostios@gmail.com>
2016-02-27 20:27:35 -0600

    add my_solution.rb

--- week-6/credit-card/my_solution.rb
+++ week-6/credit-card/my_solution.rb
@@ -3,7 +3,7 @@
 
 
 # I worked on this challenge [by myself, with: jonathan nicolas].
-# I spent [8:00-] hours on this challenge.
+# I spent [4] hours on this challenge.
 
 # Pseudocode
 
@@ -29,38 +29,44 @@
 # of exactly 16 digits
 
 # class CreditCard
-# 	attr_reader :card_digits
-# #
-# def initialize(card_digits)
-# 	@card_digits = card_digits
-# 	raise ArgumentError.new("Card number must contain 16 digits") unless @card_digits.to_s.length == 16
+
+#  attr_reader :card_digits
+
+#  def initialize(card_digits)
+#    @card_digits = card_digits
+#    raise ArgumentError.new("Card Number must contain 16 digits!") unless @card_digits.to_s.length == 16
+#  end
+
+#  def check_card
+#    array_card_digits = card_digits.to_s.split("").reverse
+#    array_card_digits.map! {|string_number| string_number.to_i}
+#    doubled_array_card_digits = array_card_digits.map.with_index{|number,index|
+#        if index % 2 == 0
+#          number
+#        else
+#          number *2
+#        end
+#    }
+#    doubled_array_card_digits = doubled_array_card_digits.join("").split("").map!{|string_number| string_number.to_i}
+#    # p doubled_array_card_digits
+
+#    sum = doubled_array_card_digits.inject(0){ |sum,x| sum + x}
+#    # p sum
+#    if sum % 10 == 0
+#      return true
+#    else
+#      return false
+#    end
+# end
 # end
 
-# def check_card 
-# 	array_card_digits = card_digits.to_s.split("").reverse
-# 	array_card_digits.map!{|string_number| string_number.to_i}
-# 	doubled_array_card_digits = array_card_digits.map.with_index do |number, index|
-# 		if index % 2 == 0
-# 			number
-# 		else
-# 			number * 2
-# 		end
-# 	end
+# jo_card = CreditCard.new(4563960122001999)
+# p jo_card.check_card
 
-# 	doubled_array_card_digits = double_array_card_digits.join("").split("").map! {|string_number| string_number.to_i}
-# 	p doubled_array_card_digits
 
-# 	sum = doubled_array_card_digits.inject(0){|sum, x| sum + x}
-# 	p sum
-# 	if sum % 10 == 0 
-# 		return true
-# 	else
-# 		return false
-# 	end
-# end
 
-# p check_card = CreditCard.new(4737029961296432)
 
+# Refactored Solution
 
 class CreditCard
 
@@ -72,48 +78,40 @@ class CreditCard
  end
 
  def check_card
-   array_card_digits = card_digits.to_s.split("").reverse
-   array_card_digits.map! {|string_number| string_number.to_i}
-   doubled_array_card_digits = array_card_digits.map.with_index{|number,index|
-       if index % 2 == 0
-         number
-       else
-         number *2
-       end
-   }
-   doubled_array_card_digits = doubled_array_card_digits.join("").split("").map!{|string_number| string_number.to_i}
-   p doubled_array_card_digits
-
-# I don't get the math on this line - I don't get how/why we are using inject
-   sum = doubled_array_card_digits.inject(0){ |sum,x| sum + x}
-   p sum
-   if sum % 10 == 0
-     return true
-   else
-     return false
-   end
-end
+ 	digits = ''
+ 	@card_digits.to_s.split('').reverse.each_with_index do |d, i|
+ 		digits += d if i%2 == 0
+ 		digits += (d.to_i*2).to_s if i%2 == 1
+ 	end
+ 	digits.split('').inject(0){|sum,d| sum + d.to_i}%10 == 0
+ end
 end
 
 jo_card = CreditCard.new(4563960122001999)
 p jo_card.check_card
-# my_card = CreditCard.new
-# my_number = 4737029961296432
-# p my_card(my_number)
-
-
-
-# Refactored Solution
-
-
-
-
-
-
 
 
 # Reflection
 
 # What was the most difficult part of this challenge for you and your pair?
+
+# I think the hardest part for me was working out exactly how to do the math
+# in such a way that I can understand it. I am more of a science/english/art
+# thinker in that sense, so the math was tricky. Especially the inject method - 
+# I furthered my understanding with that by getting a point by point
+# breakdown of exactly what inject does.
+
 # What new methods did you find to help you when you refactored?
+
+# the each_with_index method was very useful for this challenge. each_with_index is 
+# very much like each, but includes an extra argument to refer to the index, which was 
+# exactly what we needed for this case.
+
 # What concepts or learnings were you able to solidify in this challenge?
+
+# Well it was really useful for me to see more math in practice. I mean, 
+# simple math is a piece of cake but I felt like this was not beginner math at all. 
+# It is also always useful to learn more methods and techniques like the combination
+# of methods used in the refactored solution. 
+# I feel like I need to do about 40 more of these problems to understand the nuances of some
+# of this stuff, but this was a great challenge, and how cool is the Luhn algorithm in general?
